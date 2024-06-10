5f3fb00f14d0fcdde3f8862120b4f42d6dea763a
William Brandon <wbrandon@csail.mit.edu>
2024-06-10 00:52:17 -0400

    Use CARGO_MANIFEST_DIR in site.rs example

--- site_build/examples/site.rs
+++ site_build/examples/site.rs
@@ -1,10 +1,13 @@
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    site_build::SiteBuilder::new("./web/", move |site| -> Result<(), std::io::Error> {
+    let root_path = camino::Utf8PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let source_path = root_path.join("examples").join("web");
+    let output_path = root_path.join("examples").join("web-out");
+    site_build::SiteBuilder::new(&source_path, move |site| -> Result<(), std::io::Error> {
         site.copy_source_to_output("index.html", "index.html")?;
         Ok(())
     })
-    .target_dir("./web-out/")
+    .target_dir(&output_path)
     .run_from_cli()
     .await
 }
